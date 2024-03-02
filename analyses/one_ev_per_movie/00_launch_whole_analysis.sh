bf40a5c178ba7845ccdde74b11480ec18aca398f
leonardocerliani <leonardo.cerliani@gmail.com>
2024-03-02 07:49:06 +0100

    working on preprocessing one_ev_per_movie

---
+++ analyses/one_ev_per_movie/00_launch_whole_analysis.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# launch with :
+# nohup ./00_launch_whole_analysis.sh > nohup_one_ev_per_movie.out &
+
+model="one_ev_per_movie"
+
+# build the .mat files for the 1st level analysis from the onset.csv files
+Rscript 01_do_prepare_mat_${model}.R
+
+
+# first-level feat - i.e. single sub each run (~ 4 hrs)
+
+# NB: for each sub, the 8 runs are run in parallel inside
+# Therefore we run only 4 subs at a time (4x8=32 processes)
+# in order not to overwhelm the server
+
+sub_list=/data00/leonardo/RSA/sub_list.txt
+cat ${sub_list} | xargs -P 4 -I{} ./02_do_first_level_stats.sh {}
+
+# run 2nd level analyses
+./03_do_second_level.sh
+
+# run group-level analyses
+./04_do_group_level.sh
+
+#EOF
